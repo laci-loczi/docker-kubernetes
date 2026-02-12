6b340c17c544745cb88d23bd69d3ec61830a5f50
Lóczi László <loczilaci0121@gmail.com>
2026-02-12 17:27:18 +0100

    testingv5

--- app/server.js
+++ app/server.js
@@ -1,14 +1,27 @@
-// app/server.js
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
+    // Itt a változás: res.json helyett res.send HTML kóddal
     res.send(`
-        <h1>🚀 Működik a Kubernetes Cluster!</h1>
-        <p>Ezt az oldalt az Nginx szolgálja ki, a háttérben pedig a Node.js fut.</p>
-        <hr>
-        <p>Pod neve: ${process.env.HOSTNAME}</p>
+        <html>
+        <head>
+            <style>
+                body { font-family: sans-serif; background-color: #2c3e50; color: white; text-align: center; padding-top: 50px; }
+                .card { background-color: #34495e; padding: 20px; border-radius: 10px; display: inline-block; }
+                h1 { color: #2ecc71; }
+            </style>
+        </head>
+        <body>
+            <div class="card">
+                <h1>🚀 SIKER! Működik a Kubernetes!</h1>
+                <p>Ezt az oldalt a Jenkins frissítette automatikusan.</p>
+                <hr>
+                <p>Én vagyok a Pod: <strong>${process.env.HOSTNAME}</strong></p>
+            </div>
+        </body>
+        </html>
     `);
 });
 
